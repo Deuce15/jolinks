31899adf4ec3d7f26375e34756402a2b643cd902
Deuce15 <isaiahnyamu@gmail.com>
2022-12-17 23:59:32 +0300

    Email

--- jolinks/settings.py
+++ jolinks/settings.py
@@ -127,3 +127,12 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# Email Settings
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.mailtrap.io'
+EMAIL_HOST_USER = '49380bd48821cd'
+EMAIL_HOST_PASSWORD = '124a153b3cb5b8'
+EMAIL_PORT = '2525'
+EMAIL_USE_TLS = True
+EMAIL_USE_SSL = False
--- website/views.py
+++ website/views.py
@@ -1,5 +1,36 @@
 from django.shortcuts import render
+from django.core.mail import send_mail
 
 # Create your views here.
 def home(request):
     return render(request, 'home.html', {})
+
+def home(request):
+	if request.method == "POST":
+		name = request.POST['name']
+		email = request.POST['email']
+		message = request.POST['message']
+
+		data = {
+			'name':name,
+			'email':email,
+			'message':message
+		}
+		message = '''
+		New message: {}
+		From: {}
+		'''.format(data['message'], data['email'])
+		send_mail(data['email'], message, '', ['jolinkssolutions@gmail.com'])
+		
+		# send an email
+		'''
+		send_mail(
+			name, # name
+			message, # message
+			email, # from email
+			['i.nyamu5@gmail.com'], # to email
+			)
+		'''
+		return render(request, 'home.html', {'name': name})
+	else:	
+		return render(request, 'home.html', {})
